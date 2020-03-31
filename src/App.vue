f9cad44b6e8ae5fddac184a16a92581dc5e41dd3
lourencofelipe <felipe.pucsc@gmail.com>
2020-03-31 10:35:26 -0300

    Adding vue classes

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,49 @@
         </div>
       </div>
 
-      <div class="container"></div>
+      <div class="container">
+        <button class="btn btn-primary mb-3" @click="mostrar = !mostrar">Alternar</button>
+
+        <transition>
+          <div class="alert alert-primary" v-if="mostrar">
+            Animações no Vue
+          </div>
+        </transition>
+      </div>
 
     </div>
 </template>
+
+<script>
+export default {
+  data(){
+    return {
+      mostrar: true
+    }
+  }
+}
+</script>
+<style scoped>
+  
+  /* Entrada */
+  .v-enter {
+    opacity: 0;
+  }
+  .v-enter-active {
+    transition: opacity 1s;
+  }
+  .v-enter-to{
+    opacity: 1;
+  }
+  /* Saída */
+  .v-leave {
+    opacity: 1;
+  }
+  .v-leave-active {
+    transition: opacity 1s;
+  }
+  .v-leave-to{
+    opacity: 0;
+  }
+
+</style>
